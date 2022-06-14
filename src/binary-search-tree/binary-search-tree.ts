150ad1e27d444a6af2f383232f25e1bfd552c3b1
Jesse Leung <jessetleung@aol.com>
2022-06-14 21:01:34 +0900

    [Update] Finish adding binary search tree

--- src/binary-search-tree/binary-search-tree.ts
+++ src/binary-search-tree/binary-search-tree.ts
@@ -9,6 +9,91 @@
 // Postorder
 
 (function() {
+//#region Queue (taken from queue folder)
+function StaticArray<T>(length: number): T[] {
+    return Object.seal(Array<T>(length).fill(null));
+}
+
+class Queue<T> {
+    array: Array<T> | null = null;
+    front: number = 0;
+    end: number = 0;
+
+    constructor(maxQueueLength: number) {
+        this.array = StaticArray<T>(maxQueueLength);
+    }
+
+    enqueue(value: T): void {
+        if(this.end + 1 > this.array.length - 1) {
+            this.end = 0;
+            this.array[this.end] = value;
+            return;
+        }
+        this.end += this.array[this.end] === null ? 0 : 1;
+        this.array[this.end] = value;
+    }
+
+    dequeue(): T | null {
+        const removed = this.array[this.front];
+        this.array[this.front] = null;
+        if(this.front + 1 > this.array.length - 1) {
+            this.front = 0;
+            return removed;
+        }
+        this.front += 1;
+        return removed;
+    }
+
+    peek(): T | null {
+        return this.array[this.front];
+    }
+
+    indexOf(value: T) {
+        let i = this.front;
+        let index = 0;
+        while(i !== this.end) {
+            if(this.array[i] === value) {
+                return index;
+            }
+            if(i === this.array.length - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    size(): number {
+        if(this.front > this.end) {
+            const frontToLength = this.array.length - 1 - this.front;
+            return frontToLength + this.end;
+        } else if (this.front < this.end) {
+            return this.end + 1 - this.front;
+        } else {
+            return 0;
+        }
+    }
+
+    toString(): string {
+        let builder = "[";
+        let i = this.front;
+        while(i !== this.end) {
+            builder += ` ${this.array[i]}`;
+            if(i === this.array.length - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+        builder += ` ${this.array[this.end]}`;
+        builder += " ]";
+        return builder;
+    }
+}
+//#endregion
+
 //#region Binary search tree
 class TreeNode<T> {
     left: TreeNode<T> | null = null;
@@ -28,6 +113,10 @@ class BinarySearchTree<T> {
     size: number = 0;
     root: TreeNode<T> | null = null;
 
+    constructor(comparator: (value1: T, value2: T) => -1 | 0 | 1) {
+        this._comparator = comparator;
+    }
+
     isEmpty(): boolean {
         return this.size === 0;
     }
@@ -37,7 +126,7 @@ class BinarySearchTree<T> {
     }
 
     add(elem: T): void {
-        if(this.contains(elem)) {
+        if(!this.contains(elem)) {
             this.root = this._add(this.root, elem);
             this.size++;
             return;
@@ -49,7 +138,7 @@ class BinarySearchTree<T> {
         if(node === null) {
             node = new TreeNode<T>(null, null, elem);
         } else {
-            if(this._comparator(elem, node.left.value) < 0) {
+            if(this._comparator(elem, node.value) < 0) {
                 node.left = this._add(node.left, elem);
             } else {
                 node.right = this._add(node.right, elem);
@@ -129,28 +218,90 @@ class BinarySearchTree<T> {
     }
 
     traverse(order: "preOrder" | "inOrder" | "postOrder" | "levelOrder"): void {
-
+        switch(order) {
+            case "preOrder":
+                console.log("Pre-order traversal: ");
+                return this._preOrder(this.root);
+            case "postOrder":
+                console.log("Post-order traversal: ");
+                return this._postOrder(this.root);
+            case "levelOrder":
+                console.log("Level-order traversal: ");
+                return this._levelOrder(this.root);
+            case "inOrder":
+            default:
+                console.log("In-order traversal: ");
+                return this._inOrder(this.root);
+        }
     }
 
-    _preOrder(): void {
-
+    _preOrder(node: TreeNode<T>): void {
+        if(node === null) return;
+        console.log(` ${node.value} `);
+        this._preOrder(node.left);
+        this._preOrder(node.right);
     }
 
-    _inorder(): void {
-
+    _inOrder(node: TreeNode<T>): void {
+        if(node === null) return;
+        this._inOrder(node.left);
+        console.log(` ${node.value} `);
+        this._inOrder(node.right);
     }
 
-    _postOrder(): void {
-
+    _postOrder(node: TreeNode<T>): void {
+        if(node === null) return;
+        this._postOrder(node.left);
+        this._postOrder(node.right);
+        console.log(` ${node.value} `);
+    }
+    
+    _levelOrder(node: TreeNode<T>): void {
+        if(node === null) return;
+        let queue = new Queue<TreeNode<T>>(this.size);
+        queue.enqueue(this.root);
+        this._levelOrderRecurse(this.root, queue);
     }
 
-    _levelOrder(): void {
-        
+    _levelOrderRecurse(node: TreeNode<T>, queue: Queue<TreeNode<T>>) {
+        if(node === null) return;
+        while(queue.size() > 0) {
+            console.log(` ${queue.dequeue()} `);
+            queue.enqueue(node.left);
+            queue.enqueue(node.right);
+        }
     }
 
 }
 //#endregion
 
 //#region Test
+function comparator(value1: number, value2: number): -1 | 0 | 1 {
+    const diff = value1 - value2;
+    if(diff < 0) {
+        return -1;
+    } else if (diff > 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function main() {
+    let bst = new BinarySearchTree<number>(comparator);
+    bst.add(8);
+    bst.add(4);
+    bst.add(18);
+    bst.add(1);
+    bst.add(11);
+    bst.add(17);
+    bst.add(7);
+    bst.count();
+    bst.traverse("inOrder");
+    bst.traverse("preOrder");
+    bst.traverse("levelOrder");
+}
+
+main();
 //#endregion
 })();
